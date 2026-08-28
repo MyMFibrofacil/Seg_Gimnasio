6393864f8664af3555bb92852970806f5a647062
MyMFibrofacil <mymfibrofacil@gmail.com>
2026-08-28 08:20:03 -0300

    Delete css/js directory

--- css/js/config.js
+++
@@ -1,6 +0,0 @@
-// Configuración pública de la aplicación. La URL se completa después de publicar Apps Script.
-export const APP_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbybv5b5M-C3QgH-s1apKKgs1OXDQD3PGoB-swB1DDAYvmUfnoyeJKjXzq60nWB1QO-h5w/exec",
-  spreadsheetId: "1jevlhKwOlNe-LJHrhUrcVO3-w7edNC1N81L6oYqRA1k",
-  defaultPlanStartDate: ""
-};
